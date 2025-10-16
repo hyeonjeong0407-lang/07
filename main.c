fa29a0bd3a74a1e0d105f14539760dbce502ad4f
hyeonjeong0407-lang <hyeonjeong0407@gmail.com>
2025-10-16 16:13:40 +0900

    06-2

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
   #include <stdio.h> 
-int all_files;
+static int all_files; // static은 전역변수의 범위를 제한함. static을 붙이면 이 파일 내에서만 가능함
 
 extern void sub();
 
@@ -12,5 +12,6 @@ int main(void)
 {
 	sub();
 	printf("%d\n", all_files);
+	
 	return 0;
 }
